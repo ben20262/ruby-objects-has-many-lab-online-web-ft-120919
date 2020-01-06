5335f30a8c3e120ab98a7987646300228f87fe85
Benjamin Small <ben20262@aim.com>
2020-01-06 15:55:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,14 @@
 class Artist
   attr_accessor :name
+  attr_reader :songs
 
   def initialize (name)
     @name = name
   end
+
+  def add_song (song)
+    @songs << song
+  end
+
+  def add_song_by_name (song_name)
 end
